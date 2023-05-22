04a675c6bf58347f67210206f471f7c3ed9ecce1
Tommaso <tommasoclini@gmail.com>
2023-05-22 20:52:24 +0200

    Segui muro

---
+++ Programmi_Labirinto/Segui_Muro/main.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env pybricks-micropython
+from pybricks.ev3devices import Motor, UltrasonicSensor, GyroSensor
+from pybricks.parameters import Port, Direction
+from pybricks.tools import wait
+from pybricks.robotics import DriveBase
+
+from PyPID import PropIntDer
+
+# Define motor and sensor objects
+left_motor = Motor(Port.A, Direction.COUNTERCLOCKWISE)
+right_motor = Motor(Port.D, Direction.COUNTERCLOCKWISE)
+robot = DriveBase(left_motor, right_motor, 55, 92)
+right_distance_sensor = UltrasonicSensor(Port.S4)
+
+TARGET_DISTANCE = 150
+PROPORTIONAL_GAIN = .5
+DERIVATIVE_GAIN = 0.2
+INTEGRAL_GAIN = 0.05
+SPEED = 250
+TIME_DELTA = 10
+
+control = PropIntDer(PROPORTIONAL_GAIN, INTEGRAL_GAIN, DERIVATIVE_GAIN, 300)
+
+while True:
+    cte = right_distance_sensor.distance() - TARGET_DISTANCE
+
+    control.update(cte)
+
+    print(control._current_sse)
+
+    robot.drive(SPEED, control.output)
+
+    wait(TIME_DELTA)
